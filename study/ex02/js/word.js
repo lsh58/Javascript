a81e090fec1d0c82537cbc92d4a4d4f1fe84d317
lsh58 <60163978+lsh58@users.noreply.github.com>
2020-02-07 09:44:21 +0900

    Update word.js

--- study/ex02/js/word.js
+++ study/ex02/js/word.js
@@ -1,6 +1,4 @@
-
-
-function word(){
+function word() {
     //첫번째 단어를 가져온다
     //두번재 단어를 가져와서 저장한다
     //빈값인지 아닌지 체크한다
@@ -9,19 +7,22 @@ function word(){
     //같다면 첫번째 단어로 출력
     //다르다면 다시 입력하라는 경고창 출력
 
-    
-    var firstText = document.getElementsByName('js_firstText')[0].value;
-    var answer= document.getElementsByName('js_answer')[0].value;
-    
-    var lastWord = firstText.charAt(firstText.length-1);
+    var firstText = document.getElementsByName("js_firstText")[0].value;
+    var answer = document.getElementsByName("js_answer")[0].value;
+
+    var lastWord = firstText.charAt(firstText.length - 1);
     var firstWord = answer.charAt(0);
 
-    if(answer==""){alert("당신은 바보입니까?")}
-    else{ if(lastWord == firstWord){
-        document.getElementsByName('js_firstText')[0].value = answer;
-        document.getElementsByName('js_answer')[0].value = null;
-        alert("찢었다");
-        document.getElementsByName('past')[0].value+=firstText+" ";
-    }else{alert("당신은 바보입니까?")} }
-    
+    if (answer == "") {
+        alert("당신은 바보입니까?");
+    } else {
+        if (lastWord == firstWord) {
+            document.getElementsByName("js_firstText")[0].value = answer;
+            document.getElementsByName("js_answer")[0].value = null;
+            alert("찢었다");
+            document.getElementsByName("past")[0].value += firstText + " ";
+        } else {
+            alert("당신은 바보입니까?");
+        }
+    }
 }
